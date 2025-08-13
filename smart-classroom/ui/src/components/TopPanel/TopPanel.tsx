2583a7e3b3b238753c1c42a1170e03e2f6bf75a6
D Rushma <rushma.d@intel.com>
2025-08-13 09:56:18 +0530

    frontend/403

--- smart-classroom/ui/src/components/TopPanel/TopPanel.tsx
+++ smart-classroom/ui/src/components/TopPanel/TopPanel.tsx
@@ -4,15 +4,22 @@ import '../../assets/css/TopPanel.css';
 const TopPanel: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const menuRef = useRef<HTMLDivElement>(null);
+  const menuIconRef = useRef<HTMLImageElement>(null);
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   const handleClickOutside = (event: MouseEvent) => {
-  if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-    console.log('Clicked outside the menu');
-    setIsMenuOpen(false); // Close the menu when clicking outside
-  }
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target as Node) &&
+      menuIconRef.current &&
+      !menuIconRef.current.contains(event.target as Node)
+    ) {
+      console.log('Clicked outside the menu');
+      setIsMenuOpen(false); // Close the menu when clicking outside
+    }
   };
 
   useEffect(() => {
@@ -25,15 +32,16 @@ const TopPanel: React.FC = () => {
   return (
     <header className="top-panel">
       <div className="brand-slot">
-        <img src="src\assets\images\BrandSlot.svg" alt="Intel Logo" className="logo" />
+        <img src="src/assets/images/BrandSlot.svg" alt="Intel Logo" className="logo" />
         <span className="app-title">Smart Classroom Sample App</span>
       </div>
       <div className="action-slot">
         <img
-          src="src\assets\images\menu.svg"
+          src="src/assets/images/menu.svg"
           alt="Menu Icon"
           className="menu-icon"
           onClick={toggleMenu}
+          ref={menuIconRef}
         />
       </div>
       {isMenuOpen && <Menu ref={menuRef} />}
@@ -42,4 +50,3 @@ const TopPanel: React.FC = () => {
 };
 
 export default TopPanel;
-
