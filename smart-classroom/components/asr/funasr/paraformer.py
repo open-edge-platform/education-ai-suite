cf10c2b1ea143b38f1bc5e0cb0d17238aeb207cd
Cong <cong1.zhang@intel.com>
2025-08-21 11:08:03 +0800

    init funasr and ipex summarizer

--- smart-classroom/components/asr/funasr/paraformer.py
+++ smart-classroom/components/asr/funasr/paraformer.py
@@ -1,10 +1,42 @@
 from components.asr.base_asr import BaseASR
+from funasr import AutoModel
+
+FUNASR_MODEL_LIST = [
+    "iic/speech_paraformer-large-vad-punc_asr_nat-zh-cn-16k-common-vocab8404-pytorch",
+    "iic/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-online",
+    "paraformer-zh",
+    "paraformer-en",
+    "paraformer-zh-online",
+    "paraformer-zh-spk"
+]
 
 class Paraformer(BaseASR):
-   def __init__(self, model_name=..., device="cpu", revision=...):
-       # Load Paraformer model
-       pass
+    def __init__(self, model_name, device="cpu", revision="v2.0.4", ncpu=1, enable_diarization=False):
+        if model_name not in FUNASR_MODEL_LIST:
+            raise Exception(f"Invalid ASR model name {model_name}. Please refer to https://github.com/modelscope/FunASR/blob/main/model_zoo/readme.md for supported Paraformer models")
+        
+        # use same vad and punc model for different ASR models
+        if enable_diarization:
+            self.model = AutoModel(model=model_name, model_revision=revision,
+                            vad_model="fsmn-vad", vad_model_revision="v2.0.4",
+                            punc_model="iic/punc_ct-transformer_zh-cn-common-vocab272727-pytorch", punc_model_revision=revision,
+                              spk_model="cam++", spk_model_revision="v2.0.2",
+                            device=device, disable_update=True, ncpu=ncpu
+                            )
+        else:
+            self.model = AutoModel(model=model_name, model_revision=revision,
+                            vad_model="fsmn-vad", vad_model_revision="v2.0.4",
+                            punc_model="iic/punc_ct-transformer_zh-cn-common-vocab272727-pytorch", punc_model_revision=revision,
+                            #   spk_model="cam++", spk_model_revision="v2.0.2",
+                            device=device, disable_update=True, ncpu=ncpu
+                            )
+        
 
-   def transcribe(self, audio_path: str) -> str:
-       # Return transcribed text from .wav file
-       pass
+    def transcribe(self, audio_path: str) -> str:
+        # Return transcribed text from .wav file
+        res = self.model.generate(input=audio_path)
+        # res [{'key': <input>, 'text': '...', , 'timestamp': [[], [], ...], 'spk': <label>}]
+        if res.size() > 0:
+            return res[0]["text"]
+        else:
+            raise Exception("ASR transcription failed")
--- smart-classroom/components/llm/ipex/summarizer.py
+++ smart-classroom/components/llm/ipex/summarizer.py
@@ -1,10 +1,50 @@
 from components.summarizer.base_summarizer import BaseSummarizer
+from ipex_llm.transformers import AutoModelForCausalLM
+import torch
 
-class Summarizer(BaseSummarizer):
-    def __init__(self, model_name=..., revision=..., device="cpu"):
+
+class IPEXSummarizer(BaseSummarizer):
+    def __init__(self, model_name, device="xpu", model_hub="huggingface"):
+        if model_hub == "huggingface":
+            from transformers import AutoTokenizer
+        elif model_hub == "modelscope":
+            from modelscope import AutoTokenizer
+        else:
+            raise Exception(f"Invalid model_hub {model_hub}, should be huggingface or modelscope")
+        
         # Load model
-        pass
+        self.model = AutoModelForCausalLM.from_pretrained(model_name,
+                                                load_in_4bit=True,
+                                                optimize_model=True,
+                                                trust_remote_code=True,
+                                                use_cache=True,
+                                                model_hub=model_hub)
+        self.device = device
+        self.model = self.model.half().to(self.device)
 
-    def generate(self, prompt: str) -> str:
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name,
+                                              trust_remote_code=True)
+           
+
+    def generate(self, prompt: str, max_new_tokens: int = 512) -> str:
         #Summarize
-        pass
+        with torch.inference_mode():
+            text = self.tokenizer.apply_chat_template(
+                prompt,
+                tokenize=False,
+                add_generation_prompt=True
+            )
+            model_inputs = self.tokenizer([text], return_tensors="pt").to(self.device)
+  
+            generated_ids = self.model.generate(
+                model_inputs.input_ids,
+                max_new_tokens=max_new_tokens
+            )
+            torch.xpu.synchronize()
+            generated_ids = generated_ids.cpu()
+            generated_ids = [
+                output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+            ]
+
+            response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        return response
