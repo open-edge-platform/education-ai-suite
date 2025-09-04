6f349c47cf7d2cd5e9ee1a118037fb6168ef9433
unarayan <udit.narayan@intel.com>
2025-09-04 17:54:28 +0530

    optimization and fixes

--- smart-classroom/components/asr_component.py
+++ smart-classroom/components/asr_component.py
@@ -5,6 +5,8 @@ from utils.storage_manager import StorageManager
 from utils.runtime_config_loader import RuntimeConfig
 from components.asr.openai.whisper import Whisper
 from components.asr.funasr.paraformer import Paraformer
+import logging
+logger = logging.getLogger(__name__)
 
 DELETE_CHUNK_AFTER_USE =  config.pipeline.delete_chunks_after_use
 
@@ -38,18 +40,21 @@ class ASRComponent(PipelineComponent):
         project_config = RuntimeConfig.get_section("Project")
         project_path = os.path.join(project_config.get("location"), project_config.get("name"), self.session_id)
         StorageManager.save(os.path.join(project_path, "transcription.txt"), "", append=False)
-      
-        for chunk_data in input_generator:
-            chunk_path = chunk_data["chunk_path"]
-            transcribed_text = self.asr.transcribe(chunk_path, temperature=self.temperature)
+        
+        try:
+            for chunk_data in input_generator:
+                chunk_path = chunk_data["chunk_path"]
+                transcribed_text = self.asr.transcribe(chunk_path, temperature=self.temperature)
 
-            if os.path.exists(chunk_data["chunk_path"]) and DELETE_CHUNK_AFTER_USE:
-                os.remove(chunk_data["chunk_path"])
+                if os.path.exists(chunk_data["chunk_path"]) and DELETE_CHUNK_AFTER_USE:
+                    os.remove(chunk_data["chunk_path"])
 
-            StorageManager.save_async(os.path.join(project_path, "transcription.txt"), transcribed_text, append=True)
+                StorageManager.save_async(os.path.join(project_path, "transcription.txt"), transcribed_text, append=True)
 
-            yield {
-                **chunk_data,  # keep all chunk metadata
-                "text": transcribed_text
-            }
+                yield {
+                    **chunk_data,  # keep all chunk metadata
+                    "text": transcribed_text
+                }
+        finally:
+            logger.info(f"Transcription Complete: {self.session_id}")
             
--- smart-classroom/dto/transcription_dto.py
+++ smart-classroom/dto/transcription_dto.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel
 
-# Request DTOs
 class TranscriptionRequest(BaseModel):
     audio_filename: str
