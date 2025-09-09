0177d8d66969ec73a79996b92eaceda0ff5fd19c
llin60 <lin2.liu@intel.com>
2025-09-09 12:15:54 +0800

    remove model.half() for ipex summarizer

--- smart-classroom/components/llm/ipex/summarizer.py
+++ smart-classroom/components/llm/ipex/summarizer.py
@@ -59,7 +59,7 @@ class Summarizer(BaseSummarizer):
             model_hub=model_hub
         )
         self.device = device
-        self.model = self.model.half().to(self.device)
+        self.model = self.model.to(self.device)
 
         self.temperature = temperature
 
