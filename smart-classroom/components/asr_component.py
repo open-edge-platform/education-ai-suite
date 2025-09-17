e77c064a67c8c29615bb3c4b6ad7d6573f4c7854
unarayan <udit.narayan@intel.com>
2025-09-17 16:13:56 +0530

    Update asr_component.py

--- smart-classroom/components/asr_component.py
+++ smart-classroom/components/asr_component.py
@@ -30,7 +30,7 @@ class ASRComponent(PipelineComponent):
         if ASRComponent._model is None or ASRComponent._config != config:
             if provider == "openai" and "whisper" in model_name:
                 ASRComponent._model = OA_Whisper(model_name, device, None)
-            if provider == "openvino" and "whisper" in model_name:
+            elif provider == "openvino" and "whisper" in model_name:
                 ASRComponent._model = OV_Whisper(model_name, device, None)
             elif provider == "funasr" and "paraformer" in model_name:
                 ASRComponent._model = Paraformer(model_name, device.lower(), None)
